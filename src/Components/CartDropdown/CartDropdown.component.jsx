0a3c981f8a8bc96cac344746a7c82761c2ddad85
dorjepadma <dorjepadma@gmail.com>
2022-07-08 12:34:04 -0700

    added functionality to cart checkout and cart dropdown

--- src/Components/CartDropdown/CartDropdown.component.jsx
+++ src/Components/CartDropdown/CartDropdown.component.jsx
@@ -1,11 +1,18 @@
-import Button from '../Button/Button.component'
+import { useContext } from 'react';
+
+import Button from '../Button/Button.component';
+import CartItem from '../CartItem/CartItem.component';
+import { CartContext } from '../../Context/cart.context';
 
 import './CartDropdown.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
 return (
   <div className='cart-dropdown-container'>
-    <div className='cart-items'/>
+    <div className='cart-items'>
+   {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+      </div>
     <Button>CHECKOUT</Button>
     </div>  
 )
